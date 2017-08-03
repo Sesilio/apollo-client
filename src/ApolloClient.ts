1c34a117969952f6292cc9d5baa98df8eba1db09
Evans Hauser <evanshauser@gmail.com>
2017-08-03 13:48:38 -0700

    Fixed single response and added subscriptions to links

--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -5,7 +5,7 @@ import {
   Request,
 } from './transport/networkInterface';
 
-import { execute, ApolloLink } from 'apollo-link-core';
+import { execute, ApolloLink, FetchResult } from 'apollo-link-core';
 
 import {
   ExecutionResult,
@@ -235,6 +235,18 @@ export default class ApolloClient implements DataProxy {
             request,
           ) as any) as Observable<ExecutionResult>;
         }),
+        subscribe: (request: any, handler: any): string => {
+          const subscription = (execute(
+            networkInterface as ApolloLink,
+            request,
+          ) as any).subscribe({
+            next: (data: FetchResult) => handler(data.errors, data.data),
+          });
+          return '';
+        },
+        unsubscribe: (id: string): void => {
+          // wsClient.unsubscribe(id);
+        },
       };
     } else if (
       networkInterface &&
